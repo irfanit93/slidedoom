9726be7fed7a7178400929abb5b0c140bc11e73b
irfanit93 <irfanit93@gmail.com>
2018-11-18 22:14:59 +0530

    Slidedoom Angular Support

--- README.md
+++ README.md
@@ -2,6 +2,185 @@
 A responsive slide library for webpages
 Dont forget to add Slidedoom.css. It is needed inorder to display the slides correctly. 
 Visit https://irfanit93.github.io for the implementation
- Read the Source Code initialize.html for the Documentation.
- Will be improving the Documentation as soon as I have some free time.
- Any Suggestions or Feature Request are welcome.
+
+Features:
+1. Its Small. 4kb Javascript and 2Kb CSS.
+2. No External Dependencies.
+2. Integrate Slides to Your app in seconds.
+3. Easy Integration.
+4. Cross Browser Support
+5. Responsive
+6. Mobile browsers Support and Tablets Too
+7. Set Background images for slide using any colors, local image url or remote image url. Its that easy. (In backgrounds array just set any array element value as object literal {name:"<REMOTE_URL or LOCAL_URL>"}. For background colors for slide, in background array just set string as "#789564 or rgb(7,7,7)")
+
+Example:
+
+var backgrounds = [
+            { name: "https://storage.googleapis.com/cdn.magloft.com/blog/2017/02/responsive-design-1280x640.jpg" },//Slide0 background
+            { name: "https://www.w3schools.com/css/img_temp_band.jpg" },//Slide1 background
+            "#121212",//Slide2 background
+            "#888eee",//Slide3 background
+            "#a77756",//Slide4 background
+            "#444777"//Slide5 background
+        ]
+For more info, refer Documentation or source code at Github https://github.com/irfanit93/slidedoom
+
+Tip:
+
+If you want some cool glassy effect for your slide add two css classes 'glassy' and 'afterglassy': Do it like below:
+
+            <div class="slides slide0">
+                <div class="glassy" style="opacity:0.7"></div><!--set opacity from 0 to 1 to increase or decrease the glassy effect.-->
+                <div class="afterglassy"><!-- this class will make the glassy effect possible -->
+                    <h3>Slide0</h3>
+                    <div class="aboutslidedoom">Slidedoom is a lightweight and ease of use carousel library.Slidedoom is a lightweight and ease of use
+                        carousel library.Slidedoom is a lightweight and ease of use carousel library.
+                    </div>
+                </div>
+            </div>
+
+Note: Apply a background image to the slide to see the effect visually.
+
+=============================================================================================================
+
+For Angular 2+,
+
+Refer the following sample code:
+
+app.component.ts:
+
+
+import { Component, OnInit } from '@angular/core';
+
+
+import slidedoom from "slidedoom/Slidedoom.ang.min.js";
+
+ //Note that its Slidedoom.ang.min.js file for angular  ( slidedoom.js file is only for regular website or webpage)
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit {
+  
+title = 'app';
+  
+backgrounds = [
+    "rgba(0,0,0,0.8)",//Slide0 background
+   
+ "#687792",//Slide1 background
+    
+"#121212",//Slide2 background
+    
+"#888eee",//Slide3 background
+   
+ "#a77756",//Slide4 background
+    
+"#444777"//Slide5 background
+  ];
+  
+
+  
+//array of slidecontainer Height in proportion to Browser Client Height
+ 
+ // first slidecontainer height is 1/1 =100%  of Browser Client Height
+ 
+ aspectRatioHeight = [1];
+ 
+ //array of slidecontainer width in proportion to Browser Client Width
+  // first slidecontainer width is 1/1= 100%  of Browser Client Width
+  aspectRatioWidth = [1];
+
+  
+//Till now we have specified what each slidecontainer width and height
+
+  //Now we are going to  merge the slidecontainers and put it row wise
+  // First row contains only 1 slidecontainer
+ 
+ slidesgroup = [1];
+
+ 
+ //used only for css
+  //specify the maximum number of slides you are going to use in a single slidecontainer.
+  
+maxSlides = 6;
+  
+ngOnInit() {
+    
+//call slidedoom method as imported at the top  with the following arguments
+slidedoom(this.backgrounds, this.aspectRatioHeight, this.aspectRatioWidth, this.slidesgroup, this.maxSlides);
+
+
+
+}
+
+}
+
+
+app.component.css
+=================
+@import url("slidedoom/Slidedoom.min.css");
+
+app.component.html
+=================
+(Same as in previous slidedoom version)
+
+<div class="slidecontainer">
+  
+<div class="slidezimple">
+
+    <div class="slides slide0">
+   
+   <h3>Slide0</h3>
+ 
+     <div class="aboutslidedoom">Slidedoom is a lightweight and ease of use carousel library.Slidedoom is a lightweight and ease of use carousel library.Slidedoom
+        is a lightweight and ease of use carousel library.
+     
+ </div>
+  
+  </div>
+
+    <div class="slides slide1">
+      <h3>Slide1</h3>
+      <div class="aboutslidedoom">Slidedoom is a lightweight and ease of use carousel library.Slidedoom is a lightweight and ease of use carousel library.Slidedoom
+        is a lightweight and ease of use carousel library.
+      </div>
+    </div>
+    <div class="slides slide2">
+      <h3>Slide2</h3>
+      <div class="aboutslidedoom">Slidedoom is a lightweight and ease of use carousel library.Slidedoom is a lightweight and ease of use carousel library.Slidedoom
+        is a lightweight and ease of use carousel library.
+      </div>
+    </div>
+    <div class="slides slide3">
+      <h3>Slide3</h3>
+      <div class="aboutslidedoom">Slidedoom is a lightweight and ease of use carousel library.Slidedoom is a lightweight and ease of use carousel library.Slidedoom
+        is a lightweight and ease of use carousel library.
+      </div>
+    </div>
+    <div class="slides slide4">
+      <h3>Slide4</h3>
+      <div class="aboutslidedoom">Slidedoom is a lightweight and ease of use carousel library.Slidedoom is a lightweight and ease of use carousel library.Slidedoom
+        is a lightweight and ease of use carousel library.
+      </div>
+    </div>
+   
+ <div class="slides slide5">
+     
+ <h3>Slide5</h3>
+      <div class="aboutslidedoom">Slidedoom is a lightweight and ease of use carousel library.Slidedoom is a lightweight and ease of use carousel library.Slidedoom
+        is a lightweight and ease of use carousel library.
+      
+</div>
+   
+ </div>
+  
+</div>
+
+</div>
+
+<router-outlet></router-outlet>
+
+Have Fun with it ! :)
---
+++ Slidedoom.ang.js
@@ -0,0 +1,144 @@
+var slidedoom=function(backgrounds,aspectRatioHeight,aspectRatioWidth,slidesgroup,maxSlides)
+{
+(function () {
+    var slidezimple = document.getElementsByClassName("slidezimple");
+     var addSlidesStyles = document.getElementsByTagName("style")[0], slidesStyle,storeStyles = ""; 
+     var slidedoom = [], clearSlides, defaultAspectHeight = aspectRatioHeight, defaultAspectWidth = aspectRatioWidth, defaultSlidesGroup = slidesgroup; 
+    var slidescontainer = document.getElementsByClassName("slidecontainer"),slides = document.getElementsByClassName("slides"),specificSlidesLength = []; 
+     var leftarrow = document.getElementsByClassName("leftarrow"),rightarrow = document.getElementsByClassName("rightarrow");
+       var containerLength = slidescontainer.length, togglepos = []; 
+       if (document.documentElement.clientWidth < 900) {
+        aspectRatioHeight = []; aspectRatioWidth = []; slidesgroup = [];
+        for(var i=0;i<slidescontainer.length;i++)
+        {
+            aspectRatioHeight.push(1);aspectRatioWidth.push(1);slidesgroup.push(1);
+        }
+    } for (var i = 0; i < maxSlides; i++) { 
+        storeStyles += ".slide" + i + "{margin-left:" + (i * 100) + "%;}"; } 
+        slidesStyle = document.createTextNode(storeStyles); 
+        addSlidesStyles.appendChild(slidesStyle); 
+        function setBackgroundSlides() { for (var i = 0; i < slides.length; i++) { if (typeof backgrounds[i] == "string") { slides[i].style.background = backgrounds[i]; } else if (typeof backgrounds[i] == "object") { slides[i].style.background = "url(" + backgrounds[i].name + ") 0% 0%/100% 100% no-repeat"; } } } 
+        for (var i = 0; i < containerLength; i++) {
+             specificSlidesLength.push(slidescontainer[i].getElementsByClassName("slides").length); 
+             slidedoom.push(0); 
+             togglepos.push(0); 
+            } 
+            function recalculate() {
+                 for (var i = 0; i < containerLength; i++) {
+                      slidescontainer[i].style.height = (document.documentElement.clientHeight / aspectRatioHeight[i]) + "px"; slidescontainer[i].style.width = (100 / aspectRatioWidth[i]) + "%"; for (var j = 0; j < specificSlidesLength[i]; j++) { slidescontainer[i].getElementsByClassName("slides")[j].style.height = (document.documentElement.clientHeight / aspectRatioHeight[i]) + "px"; } } 
+                    } recalculate(); function recalculateScrollHeight() {
+        var k = 0, slidesContainerCount = 0, finalScrollHeight = 0, slidesContainerIteration = 0, fullContainer = 0;
+         for (var i = 0; i < slidesgroup.length; i++) {
+            console.log("slidesgroup" + slidesgroup[i]); fullContainer += slidesgroup[i]; if (slidesgroup[i] > 1) {
+                for (var j = 0; j < slidesgroup[i]; j++) {
+                    if (slidescontainer[slidesContainerIteration].scrollHeight > finalScrollHeight) { finalScrollHeight = slidescontainer[slidesContainerIteration].scrollHeight; }
+                    slidesContainerIteration++; if (j == (slidesgroup[i] - 1)) {
+                        for (var k = 0; k < slidesgroup[i]; k++) {
+                            slidescontainer[slidesContainerCount].style.height = finalScrollHeight + "px"; slidesContainerCount++;
+                        }
+                    }
+                }
+            }
+            else { console.log('fullContainer',fullContainer);slidescontainer[fullContainer - 1].style.height = slidescontainer[fullContainer - 1].scrollHeight + "px"; slidesContainerIteration++; slidesContainerCount++; } finalScrollHeight = 0;
+        }
+        for (var i = 0; i < slidescontainer.length; i++) { for (var j = 0; j < slidescontainer[i].getElementsByClassName("slides").length; j++) { slidescontainer[i].getElementsByClassName("slides")[j].style.height = slidescontainer[i].scrollHeight + "px"; } } setBackgroundSlides();
+    } recalculateScrollHeight();
+    function moveslidecommon(val, cid, pos, containerindex) {
+        var containerid = cid, stop = val;
+        return {
+            containers: function () {
+                if (Math.abs(togglepos[containerindex]) != 0 && pos == "left") {
+                    if (togglepos[containerindex] == 0) {
+                        togglepos[containerindex] = 0;
+                    }
+                    else {
+                        togglepos[containerindex] += 100;
+                    }
+                }
+                if (pos == "right") {
+                    if (Math.abs(togglepos[containerindex]) == stop) {
+                        togglepos[containerindex] = -stop;
+                    }
+                    else
+                        togglepos[containerindex] -= 100;
+                }
+                containerid.style.webkitTransform = "translate(" + togglepos[containerindex] + "%)";
+                containerid.style.webkitTransitionDuration = "0.3s";
+                containerid.style.webkitTransitionTimingFunction = "linear";
+                containerid.style.oTransform = "translate(" + togglepos[containerindex] + "%)";
+                containerid.style.oTransitionDuration = "0.3s";
+                containerid.style.oTransitionTimingFunction = "linear";
+                containerid.style.transform = "translate(" + togglepos[containerindex] + "%)";
+                containerid.style.transitionDuration = "0.3s";
+                containerid.style.transitionTimingFunction = "linear";
+                return togglepos;
+            }
+        };
+    }
+    for (var i = 0; i < rightarrow.length; i++) {
+        (function () {
+            var mscommon = moveslidecommon(((rightarrow[i].parentNode.children[1].getElementsByClassName("slides").length) - 1) * 100, rightarrow[i].parentNode.children[1], "right", i).containers;
+            rightarrow[i].addEventListener("click", function () {
+                clearInterval(clearSlides);
+                mscommon();
+            });
+        })();
+    }
+    for (var i = 0; i < leftarrow.length; i++) {
+        (function () {
+            var mscommon = moveslidecommon(((leftarrow[i].parentNode.children[1].getElementsByClassName("slides").length) - 1) * 100, leftarrow[i].parentNode.children[1], "left", i).containers;
+            leftarrow[i].addEventListener("click", function () {
+                clearInterval(clearSlides);
+                mscommon();
+            });
+        })();
+    }
+    clearSlides = setInterval(function () {
+        for (var i = 0; i < containerLength; i++) {
+            if (Math.abs(slidedoom[i]) == ((specificSlidesLength[i] - 1) * 100)) {
+                slidedoom[i] = 100;
+            }
+            slidedoom[i] -= 100;
+        }
+        for (var i = 0; i < slidezimple.length; i++) {
+            slidezimple[i].style.webkitTransform = "translate(" + slidedoom[i] + "%)";
+            slidezimple[i].style.webkitTransitionDuration = "0.3s";
+            slidezimple[i].style.webkitTransitionTimingFunction = "linear";
+            slidezimple[i].style.oTransform = "translate(" + (slidedoom[i]) + "%)";
+            slidezimple[i].style.oTransitionDuration = "0.3s";
+            slidezimple[i].style.oTransitionTimingFunction = "linear";
+            slidezimple[i].style.transform = "translate(" + (slidedoom[i]) + "%)";
+            slidezimple[i].style.transitionDuration = "0.3s";
+            slidezimple[i].style.transitionTimingFunction = "linear";
+        }
+    }, 3000);
+    var headadjust = document.getElementsByClassName("headadjust");
+    function adjustHeader() {
+        if(headadjust[0])
+        headadjust[1].style.marginTop = headadjust[0].scrollHeight + "px";
+    }
+    adjustHeader();
+    window.addEventListener('resize',function () {
+        adjustHeader();
+        if (document.documentElement.clientWidth < 900) {
+            for (var i = 0; i < containerLength; i++) {
+                specificSlidesLength.push(slidescontainer[i].getElementsByClassName("slides").length); 
+                slidedoom.push(0); 
+                togglepos.push(0); 
+               }
+        }
+        else {
+            aspectRatioHeight = defaultAspectHeight;
+            aspectRatioWidth = defaultAspectWidth;
+            slidesgroup = defaultSlidesGroup;
+        }
+        recalculate();
+        recalculateScrollHeight();
+    });
+}
+)();
+/* window.addEventListener('load', function () {
+    
+}); */
+}
+module.exports=slidedoom;
